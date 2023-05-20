414b49e54045c9d1d1165be4b876fd57b821147a
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-20 04:45:09 +0300

    Add dev dependencies

--- assignment 1/main.js
+++ assignment 1/main.js
@@ -1,21 +1,26 @@
 function fibs(n) {
-    if (n===1) return [0]
-    if (n===2) return [0,1]
-    const fibArray=[0,1]
-    for(let i=0;i<n-2;i+=1){
-        fibArray.push(fibArray[fibArray.length-1]+fibArray[fibArray.length-2])
-    }
-    return  fibArray
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  const fibArray = [0, 1];
+  for (let i = 0; i < n - 2; i += 1) {
+    fibArray.push(
+      fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]
+    );
+  }
+  return fibArray;
 }
 
-console.log(fibs(8))
+console.log(fibs(8));
 
-function fibsRec(n){
-    if (n===1) return [0]
-    if (n===2) return [0,1]
-    const smallerArray=fibsRec(n-1)
-    return smallerArray.concat([smallerArray[smallerArray.length-1] + smallerArray[smallerArray.length-2]])
+function fibsRec(n) {
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  const smallerArray = fibsRec(n - 1);
+  return smallerArray.concat([
+    smallerArray[smallerArray.length - 1] +
+      smallerArray[smallerArray.length - 2],
+  ]);
 }
 // could replace smallerArray.length with n-1
 
-console.log(fibsRec(8))
+console.log(fibsRec(8));
